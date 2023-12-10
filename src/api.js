b404663d8e92bc27b94a7724ca922f3605c3a05f
jeffinmeister <jeffinpoulose2@gmail.com>
2023-12-11 00:32:42 +0530

    netlify

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const serverless = require('serverless-http');
+
 const path = require('path');
 require('dotenv').config()
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-const userRouter = require('./routes/users');
-const taskRouter = require('./routes/tasks');
+const userRouter = require('../routes/users');
+const taskRouter = require('../routes/tasks');
 
 const app = express();
 
@@ -18,7 +20,7 @@ app.use(cookieParser());
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DB}/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-app.use('/user/', userRouter);
-app.use('/tasks/', taskRouter);
+app.use('/.netlify/functions/app/user/', userRouter);
+app.use('/.netlify/functions/app/tasks/', taskRouter);
 
-module.exports = app;
+module.exports.handler = serverless(app);
